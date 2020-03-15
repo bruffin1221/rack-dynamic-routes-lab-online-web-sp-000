7acb3bfbad52aeef901dcf90fb21a5f01d5df4da
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-15 22:18:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ def call(env)
     resp.write "#{item.price}\n"
   end
 elsif req.path=="/test"
-    resp.write "Item not found"
+    resp.write "Route not found"
     resp.status=400
   end
   resp.finish
