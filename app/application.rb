9380a25ba78ccce9ccd92e241e5f944bb95a0e96
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-15 23:05:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,15 +6,14 @@ def call(env)
   resp=Rack::Response.new
   req=Rack::Request.new(env)
 
-
-  if req.path=="/test/"
-    resp.write "Route not found"
-    resp.status=400
-  elsif req.path.match("/items/")
+  if req.path.match("/items/")
 
     item_price=req.path("/items/").last
     @@items.find{|i| i.price==item_price}
     resp.write item.price
+  elsif req.path=="/test/"
+    resp.write "Route not found"
+    resp.status=400
   end
   resp.finish
 end
