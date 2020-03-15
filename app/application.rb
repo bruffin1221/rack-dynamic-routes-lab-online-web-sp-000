060f6072b3f2e15c747c7b6982ed6783ff1ca8b2
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-15 23:24:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ def call(env)
     stuff=req.path.split("/items/").last
 
     @@items.find{|i| i.price==stuff}
-    resp.write item.price
+    resp.write "#{stuff}/n"
   elsif resp.write "Route not found"
     resp.status=404
   end
